777130c29dff19e37b4bf6c68a35026e5346f764
Charanrajesh <36912699+Charanrajesh@users.noreply.github.com>
2019-03-23 22:43:03 +0530

    Add files via upload

---
+++ efgp.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="description" content="Vali is a responsive and free admin theme built with Bootstrap 4, SASS and PUG.js. It's fully customizable and modular.">
+  
+    
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Vali Admin">
+    <meta property="og:title" content="Vali - Free Bootstrap 4 admin theme">
+    <meta property="og:url" content="http://pratikborsadiya.in/blog/vali-admin">
+    <meta property="og:image" content="http://pratikborsadiya.in/blog/vali-admin/hero-social.png">
+    <meta property="og:description" content="Vali is a responsive and free admin theme built with Bootstrap 4, SASS and PUG.js. It's fully customizable and modular.">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css1/style.css" />
+    <title>AYUSH</title>
+  </head>
+  <body class="app sidebar-mini">
+ <?php
+  error_reporting(0);
+	require('db.php');
+    
+    if (isset($_REQUEST['username'])){
+		$username = stripslashes($_REQUEST['username']); 
+		$username = mysqli_real_escape_string($con,$username); 
+	
+		$email = mysqli_real_escape_string($con,$email);
+		$password = stripslashes($_REQUEST['password']);
+		$password = mysqli_real_escape_string($con,$password);
+
+		$trn_date = date("Y-m-d H:i:s");
+		
+		 $query = mysqli_query($con, "SELECT * FROM entrepreneur WHERE username = '".$username. "'");
+  if(mysqli_num_rows(  $query) > 0){
+	   $query = "update users SET password ='".md5($password)."' where username = '$username'";
+        $result = mysqli_query($con,$query);
+		
+   
+}
+else{
+
+ echo "<div class='form'><h3>!!!!!!!!!!! Please check your username</h3><br/>Click here to try again <a href='efgp.php'>Update password</a></div>";
+
+}
+        if($result){
+  
+			
+			
+			
+            echo "<div class='form'><h3>password updates successfully</h3><br/>Click here to <a href='entlogin.php'>Login</a></div>";
+        
+    }}
+	else{
+?>
+    
+    <header class="app-header"><a class="app-header__logo" href="http://localhost/pocketclinic/">AYUSH</a>
+      <a class="app-sidebar__toggle" href="#" data-toggle="sidebar"></a>
+      
+      
+    </header>
+    <!-- Sidebar menu-->
+ 
+   <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
+    <aside class="app-sidebar">
+      
+      </div>
+      <ul class="app-menu">
+        <li><a class="app-menu__item active" href="index.php"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">HOME</span></a></li>
+		 <li><a class="app-menu__item active" href="index.php"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">KNOW MORE</span></a></li
+		  <li><a class="app-menu__item active" href="index.php"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">CONTACT US</span></a></li>
+         </ul>
+		 </aside>
+
+  
+          </ul>
+    <main class="app-content">
+      <div class="app-title">
+        <div> 
+          <h1><i class="fa fa-dashboard"></i>UPDATE PASSWORD / YOUR USERNAME BECOME YOUR USERID</h1>
+          
+        </div>
+       
+      </div>
+     
+      
+      <div class="row">
+        <div class="col-md-6">
+          <div class="tile">
+ <div class="form">
+<h1>UPDATE PASSWORD</h1>
+  <div class="form">
+<form name="registration" action="" method="post">
+<input type="text" name="username" placeholder="Username" required />
+<input type="email" name="email" placeholder="Email" required />
+<input type="password" name="password"  id="password" placeholder="Password" required />
+<input type="password" name="password"   id="confirm_password" placeholder="Confirm Password" required />
+<script>
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+</script>
+
+<input type="submit" name="submit" value="Register" />
+</form></div>
+	<?php  } ?>
+          </div>
+        </div>
+		
+    </main>
+    
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+    
+    <script src="js/plugins/pace.min.js"></script>
+    
+    <script type="text/javascript" src="js/plugins/chart.js"></script>
+    <script type="text/javascript">
+   
+      var pdata = [
+      	{
+      		value: 300,
+      		color: "#46BFBD",
+      		highlight: "#5AD3D1",
+      		label: "Complete"
+      	},
+      	{
+      		value: 50,
+      		color:"#F7464A",
+      		highlight: "#FF5A5E",
+      		label: "In-Progress"
+      	}
+      ]
+      
+      var ctxl = $("#lineChartDemo").get(0).getContext("2d");
+      var lineChart = new Chart(ctxl).Line(data);
+      
+      var ctxp = $("#pieChartDemo").get(0).getContext("2d");
+      var pieChart = new Chart(ctxp).Pie(pdata);
+    </script>
+    
+    <script type="text/javascript">
+      if(document.location.hostname == 'pratikborsadiya.in') {
+      	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      	ga('create', 'UA-72504830-1', 'auto');
+      	ga('send', 'pageview');
+      }
+    </script>
+  </body>
+</html>
